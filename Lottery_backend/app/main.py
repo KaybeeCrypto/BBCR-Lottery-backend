43c4658c34223b5c00e39dd88c0f60a91666ca4c
Kaybee <48563227+KaybeeCrypto@users.noreply.github.com>
2026-01-30 16:11:40 +0100

    Add files via upload

--- Lottery_backend/app/main.py
+++ Lottery_backend/app/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from .database import engine
 from .models import Base
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Lottery Backend")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://kaybeecrypto.github.io",  # replace with your GitHub Pages domain root
+    ],
+    allow_credentials=False,
+    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_headers=["*"],
+)
+
 
 @app.on_event("startup")
 def startup_event():
